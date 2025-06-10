93fae022e665c9a50a07316b67864c06784909a1
SteelBall_Debug <parthpatil1202@gmail.com>
2025-06-10 20:36:50 +0530

    Successfully implemented Tree-view for viewing Transactions

--- Tabular_view.py
+++ Tabular_view.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-from tkinter import font
+import tkinter as tk
+from tkinter import font, ttk
 from tkcalendar import DateEntry
 import json
 # github is annoying
@@ -12,13 +13,79 @@ class Table(ctk.CTkScrollableFrame):
         super().__init__(master=master)
         self.create_grid()
 
+        # Transactor inside a frame
         self.transactor = TransactionEngine(self)
-        self.transactor.grid(row=0, column=0, sticky="ns", pady=10)
+        self.transactor.grid(row=0, column=0, sticky="nsw", pady=10)
+
+        # Label
+        self.label = ctk.CTkLabel(self, text="Transaction Table", font=("Segoe UI", 24, "bold"))
+        self.label.grid(row=1, column=0, sticky="n", pady=10)
+
+        # Refresh Button
+        self.transactions = None
+        self.refresh = ctk.CTkButton(self, text="↻", font=("", 24, "bold"), command=self.refresh_table)
+        self.refresh.grid(row=1, column=0, sticky="nw", pady=10, padx=10)
+
+        # Table view frame
+        self.table_frame = ctk.CTkFrame(self, fg_color="black")
+        self.table_frame.grid(row=2, column=0, sticky='nsew', pady=10)
+
+        # Table view variables
+        self.columns = ("Date", "User", "Type", "Category","Amount", "Note")
+        self.tree = ttk.Treeview(self.table_frame, columns=self.columns, show="headings", height=10)
+        self.setup_table()
 
     def create_grid(self):
-        self.rowconfigure(0, weight=25)
-        self.rowconfigure(1, weight=50)
+        self.rowconfigure(0, weight=20)
+        self.rowconfigure(1, weight=10)
+        self.rowconfigure(2, weight=70)
         self.columnconfigure(0, weight=1)
+
+    def setup_table(self):
+        for column in self.columns:
+            self.tree.heading(column, text=column)
+            self.tree.column(column, anchor="center", width=200)
+
+        # Style Treeview
+        style = ttk.Style()
+        style.theme_use("default")
+        # enlarge row size and font
+        style.configure("Treeview",
+                        background="#2b2b2b",
+                        foreground="white",
+                        rowheight=40,
+                        fieldbackground="#2b2b2b",
+                        font=("Segoe UI", 14))
+        # style Header cells
+        style.configure("Treeview.Heading",
+                        background="#1054c2",  # <-- Header background
+                        foreground="white",  # <-- Header text color
+                        font=("Segoe UI", 14, "bold"))
+        style.configure("Treeview.Heading", font=("Segoe UI", 16, "bold"))  # enlarge heading fonts
+        style.map('Treeview', background=[('selected', '#1f6aa5')])
+
+        with open("Transactions.json", "r+") as f:
+            file_data = json.load(f)
+            self.transactions = file_data["Transactions"]
+
+            for entry in self.transactions:
+                row = [entry["date"], entry["user"], entry["type"], entry["category"], entry["amount"], entry["note"]]
+                self.tree.insert("", tk.END, values=row)
+
+        self.tree.pack(fill="both", expand=True)
+
+    def refresh_table(self):
+        with open("Transactions.json", "r+") as f:
+            file_data = json.load(f)
+            transactions = file_data["Transactions"]
+
+            for entry in transactions:
+                if entry in self.transactions:
+                    print("no new entries")
+                else:
+                    row = [entry["date"], entry["user"], entry["type"], entry["category"], entry["amount"], entry["note"]]
+                    self.transactions.append(entry)
+                    self.tree.insert("", tk.END, values=row)
 
 
 class TransactionEngine(ctk.CTkFrame):
@@ -101,8 +168,6 @@ class TransactionEngine(ctk.CTkFrame):
         # check for missing values
         if user_value in self.user_values and type_value in self.types and category in self.categories:
             if amount >= 0:
-                # print(f"user: {user_value} \namount: {amount} \ntype: {type_value} \ncategory: {category} \ndate:
-                # {date} \n"f"note: {note}")
                 with open("Transactions.json", "r+") as f:
                     file_data = json.load(f)
 
@@ -111,6 +176,7 @@ class TransactionEngine(ctk.CTkFrame):
                         "user": user_value,
                         "type": type_value,
                         "category": category,
+                        "amount": amount,
                         "note": note
                     }
 
